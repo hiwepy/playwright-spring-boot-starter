9b3123b160a11996079778d6b4a13bc06c7d2d45
wandl <hiwepy@163.com>
2023-06-15 13:26:30 +0800

    优化

--- src/main/java/com/microsoft/playwright/spring/boot/PlaywrightAutoConfiguration.java
+++ src/main/java/com/microsoft/playwright/spring/boot/PlaywrightAutoConfiguration.java
@@ -14,14 +14,19 @@ import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Objects;
 
 @Configuration
 @ConditionalOnClass({ Playwright.class, PooledObjectFactory.class })
 @EnableConfigurationProperties(PlaywrightProperties.class)
 @Slf4j
 public class PlaywrightAutoConfiguration {
+
+    protected static final PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
 
     @Bean(name = "browserContextPool")
     @ConditionalOnMissingBean
@@ -31,6 +36,7 @@ public class PlaywrightAutoConfiguration {
         BrowserContextPooledObjectFactory factory = new BrowserContextPooledObjectFactory();
         // 创建 GenericObjectPoolConfig 对象，并进行必要的配置
         GenericObjectPoolConfig<BrowserContext> poolConfig = new GenericObjectPoolConfig<>();
+        this.copyProperties(playwrightProperties.getPagePool(), poolConfig);
         poolConfig.setJmxEnabled(Boolean.FALSE);
         poolConfig.setJmxNameBase(JmxBeanUtils.getObjectName(BrowserContextPool.class));
         // 创建 BrowserContextPool 对象
@@ -46,14 +52,35 @@ public class PlaywrightAutoConfiguration {
         BrowserPagePooledObjectFactory factory = new BrowserPagePooledObjectFactory(browserContextPool);
         // 创建 GenericObjectPoolConfig 对象，并进行必要的配置
         GenericObjectPoolConfig<Page> poolConfig = new GenericObjectPoolConfig<>();
+        this.copyProperties(playwrightProperties.getPagePool(), poolConfig);
         poolConfig.setJmxEnabled(Boolean.FALSE);
         poolConfig.setJmxNameBase(JmxBeanUtils.getObjectName(BrowserPagePool.class));
-
         // 创建 BrowserPagePool 对象，传入 factory 和 poolConfig
         BrowserPagePool pagePool = new BrowserPagePool(factory, poolConfig);
         return pagePool;
     }
 
-
+    protected void copyProperties(PlaywrightProperties.ObjectPoolConfig source, GenericObjectPoolConfig poolConfig){
+        if (Objects.isNull(source) || Objects.isNull(poolConfig)){
+            return;
+        }
+        map.from(source.isBlockWhenExhausted()).to(poolConfig::setBlockWhenExhausted);
+        map.from(source.getDurationBetweenEvictionRuns()).to(poolConfig::setTimeBetweenEvictionRuns);
+        map.from(source.getEvictionPolicyClassName()).to(poolConfig::setEvictionPolicyClassName);
+        map.from(source.getEvictorShutdownTimeoutDuration()).to(poolConfig::setEvictorShutdownTimeout);
+        map.from(source.isFairness()).to(poolConfig::setFairness);
+        map.from(source.isLifo()).to(poolConfig::setLifo);
+        map.from(source.getMaxWaitDuration()).to(poolConfig::setMaxWait);
+        map.from(source.getMaxIdle()).to(poolConfig::setMaxIdle);
+        map.from(source.getMaxTotal()).to(poolConfig::setMaxTotal);
+        map.from(source.getMinEvictableIdleDuration()).to(poolConfig::setMinEvictableIdleTime);
+        map.from(source.getMinIdle()).to(poolConfig::setMinIdle);
+        map.from(source.getNumTestsPerEvictionRun()).to(poolConfig::setNumTestsPerEvictionRun);
+        map.from(source.getSoftMinEvictableIdleDuration()).to(poolConfig::setSoftMinEvictableIdleTime);
+        map.from(source.isTestOnBorrow()).to(poolConfig::setTestOnBorrow);
+        map.from(source.isTestOnCreate()).to(poolConfig::setTestOnCreate);
+        map.from(source.isTestOnReturn()).to(poolConfig::setTestOnReturn);
+        map.from(source.isTestWhileIdle()).to(poolConfig::setTestWhileIdle);
+    }
 
 }
--- src/main/java/com/microsoft/playwright/spring/boot/PlaywrightProperties.java
+++ src/main/java/com/microsoft/playwright/spring/boot/PlaywrightProperties.java
@@ -18,8 +18,11 @@ package com.microsoft.playwright.spring.boot;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
 import lombok.Data;
+import org.apache.commons.pool2.impl.EvictionPolicy;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
 
 /**
  * 
@@ -31,8 +34,47 @@ public class PlaywrightProperties {
 
 	public static final String PREFIX = "playwright";
 
-	private GenericObjectPoolConfig<BrowserContext> browserPoolConfig = new GenericObjectPoolConfig<>();
+	private ObjectPoolConfig browserPool = new ObjectPoolConfig();
 
-	private GenericObjectPoolConfig<Page> pagePoolConfig = new GenericObjectPoolConfig<>();
+	private ObjectPoolConfig pagePool = new ObjectPoolConfig();
+
+	@Data
+	public static class ObjectPoolConfig {
+
+		private boolean blockWhenExhausted = GenericObjectPoolConfig.DEFAULT_BLOCK_WHEN_EXHAUSTED;
+
+		private Duration durationBetweenEvictionRuns = GenericObjectPoolConfig.DEFAULT_TIME_BETWEEN_EVICTION_RUNS;
+
+		private Duration evictorShutdownTimeoutDuration = GenericObjectPoolConfig.DEFAULT_EVICTOR_SHUTDOWN_TIMEOUT;
+
+		private String evictionPolicyClassName = GenericObjectPoolConfig.DEFAULT_EVICTION_POLICY_CLASS_NAME;
+
+		private boolean fairness = GenericObjectPoolConfig.DEFAULT_FAIRNESS;
+
+		private boolean lifo = GenericObjectPoolConfig.DEFAULT_LIFO;
+
+		private Duration maxWaitDuration = GenericObjectPoolConfig.DEFAULT_MAX_WAIT;
+
+		private int maxTotal = GenericObjectPoolConfig.DEFAULT_MAX_TOTAL;
+
+		private int maxIdle = GenericObjectPoolConfig.DEFAULT_MAX_IDLE;
+
+		private int minIdle = GenericObjectPoolConfig.DEFAULT_MIN_IDLE;
+
+		private Duration minEvictableIdleDuration = GenericObjectPoolConfig. DEFAULT_MIN_EVICTABLE_IDLE_DURATION;
+
+		private Duration softMinEvictableIdleDuration = GenericObjectPoolConfig.DEFAULT_SOFT_MIN_EVICTABLE_IDLE_DURATION;
+
+		private int numTestsPerEvictionRun = GenericObjectPoolConfig.DEFAULT_NUM_TESTS_PER_EVICTION_RUN;
+
+		private boolean testOnCreate = GenericObjectPoolConfig.DEFAULT_TEST_ON_CREATE;
+
+		private boolean testOnBorrow = GenericObjectPoolConfig.DEFAULT_TEST_ON_BORROW;
+
+		private boolean testOnReturn = GenericObjectPoolConfig.DEFAULT_TEST_ON_RETURN;
+
+		private boolean testWhileIdle = GenericObjectPoolConfig.DEFAULT_TEST_WHILE_IDLE;
+
+	}
 
 }
